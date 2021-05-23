29b3fb434368f260df0f10e2eb810a3e984e1971
FranciscoDadone <dadonefrancisco@hotmail.com.ar>
2021-05-22 23:16:49 -0300

    editando el template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { companyName: '01Computación' });
 });
 
 module.exports = router;
